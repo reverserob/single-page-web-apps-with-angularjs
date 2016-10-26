b28c7eccdc91bbf565f0e7d00adb239e537b5129
robertarandazzo <randazzo@olomedia.it>
2016-10-26 21:17:14 +0200

    module1-solution

---
+++ module1-solution/app.js
@@ -0,0 +1,43 @@
+(function () {
+    angular.module("LunchCheck",[])
+        .controller("LunchCheckController", LunchCheckController);
+
+    LunchCheckController.$inject=["$scope"];
+
+    function LunchCheckController($scope) {
+        $scope.items="";
+        $scope.result="";
+        $scope.textstyle="";
+        $scope.borderstyle="";
+        $scope.checkIfTooMuch = function() {
+            if($scope.items === " "){
+                $scope.textstyle={"color":"red"};
+                $scope.borderstyle={"border":"1px solid red"};
+                $scope.result="Please enter data first";
+            } else{
+                var itemsArray = $scope.items.split(',');
+              //  console.log(itemsArray)
+                var nonEmptyArray=[];
+
+                for(var i=0; i<itemsArray.length; i++){
+
+
+                    if (itemsArray[i].trim().length) {
+                        console.log('n.' + i + ':' + itemsArray[i])
+                        nonEmptyArray.push(itemsArray);
+                    }
+                }
+                if(nonEmptyArray.length <= 3){
+                    $scope.textstyle={"color":"green"};
+                    $scope.borderstyle={"border":"1px solid green"};
+                    $scope.result="Enjoy!";
+                } else{
+                    $scope.textstyle={"color":"green"};
+                    $scope.borderstyle={"border":"1px solid green"};
+                    $scope.result="Too much!";
+                }
+            }
+        };
+
+    };
+})();
